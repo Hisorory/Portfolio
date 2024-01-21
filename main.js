89302229330b2422e55708f97412aa126054070d
HelaSofera <153563760+Hisorory@users.noreply.github.com>
2024-01-21 18:07:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,29 +13,3 @@ function carousel() {
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
-var slideIndex1 = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex1 += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex1 = n);
-}
-
-function showDivs(n) {
-  var y;
-  var x = document.getElementsByClassName("mySlides1");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = x.length}
-  for (y = 0; i < x.length; i++) {
-    x[y].style.display = "none";  
-  }
-  for (y = 0; i < dots.length; i++) {
-    dots[y].className = dots[y].className.replace(" w3-white", "");
-  }
-  x[slideIndex1-1].style.display = "block";  
-  dots[slideIndex1-1].className += " w3-white";
-}
